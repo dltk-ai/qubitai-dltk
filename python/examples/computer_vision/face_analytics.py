8c3a077a650815c099591a926af67ce189850649
Vishnu Peesapati <vishnu@qubitai.tech>
2021-01-29 20:22:59 +0530

    added face-detection code and example

--- python/examples/computer_vision/face_analytics.py
+++ python/examples/computer_vision/face_analytics.py
@@ -4,4 +4,3 @@ dltkai = dltk_ai.DltkAiClient('552fd7cc-6aed-411d-96d3-2d02a62d375b')
 response = dltkai.face_analytics(image_path='../../img/fd-actual-img.jpg', dlib=False, opencv=True, azure=False,
                                  mtcnn=False,
                                  output_types=["json"])
-print(response)
