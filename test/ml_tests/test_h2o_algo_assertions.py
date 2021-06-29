4e338a87a4fa8cbbe05553c557da8f3633861639
Vivek Kumar Yadav <46562067+vivekkya@users.noreply.github.com>
2021-06-29 18:03:08 +0530

    new testcases for default value checks

--- test/ml_tests/test_h2o_algo_assertions.py
+++ test/ml_tests/test_h2o_algo_assertions.py
@@ -8,6 +8,51 @@ class TestH2OClassificationAlgo(unittest.TestCase):
 
     def setUp(self):
         self.library = "h2o"
+        self.service = "classification"
+        pass
+
+    def test_naivebayesbinomial_1(self):
+        algorithm = "NaiveBayesBinomial"
+        params = {'auc_type': 'auto', 'balance_classes': False, 'compute_metrics': True, 'eps_prob': 30, 'eps_sdev': 0,
+                  'fold_assignment': 'auto', 'gainslift_bins': -1, 'ignore_const_cols': True,
+                  'keep_cross_validation_fold_assignment': False, 'keep_cross_validation_models': True,
+                  'keep_cross_validation_predictions': False, 'laplace': 0, 'max_after_balance_size': 5,
+                  'max_confusion_matrix_size': 0, 'max_runtime_secs': 0, 'min_prob': 0.001, 'min_sdev': 0.001,
+                  'score_each_iteration': False, 'seed': -1}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_deeplearning_1(self):
+        algorithm = "DeepLearning"
+        params = {'activation': 'rectifier', 'adaptive_rate': True, 'auc_type': 'auto', 'autoencoder': False,
+                  'average_activation': 0.0, 'balance_classes': False, 'categorical_encoding': 'auto',
+                  'classification_stop': 0, 'col_major': False, 'diagnostics': True, 'distribution': 'auto',
+                  'elastic_averaging': False, 'elastic_averaging_moving_rate': 0.9,
+                  'elastic_averaging_regularization': 0.001, 'epochs': 10, 'epsilon': 1e-08,
+                  'export_weights_and_biases': False, 'fast_mode': True, 'fold_assignment': 'auto',
+                  'force_load_balance': True, 'huber_alpha': 0.9, 'ignore_const_cols': True,
+                  'initial_weight_distribution': 'uniform_adaptive', 'initial_weight_scale': 0,
+                  'input_dropout_ratio': 0, 'keep_cross_validation_fold_assignment': False,
+                  'keep_cross_validation_models': True, 'keep_cross_validation_predictions': False, 'l1': 0, 'l2': 0,
+                  'loss': 'automatic', 'max_after_balance_size': 5.0, 'max_categorical_features': 2147483647,
+                  'max_confusion_matrix_size': 20, 'max_runtime_secs': 0.0, 'max_w2': 3.4028235e+38,
+                  'mini_batch_size': 1, 'missing_values_handling': 'mean_imputation', 'momentum_ramp': 1000000,
+                  'momentum_stable': 0, 'momentum_start': 0, 'nesterov_accelerated_gradient': True,
+                  'overwrite_with_best_model': True, 'quantile_alpha': 0.5, 'quiet_mode': False, 'rate': 0.005,
+                  'rate_annealing': 1e-06, 'rate_decay': 1, 'regression_stop': 1e-06, 'replicate_training_data': True,
+                  'reproducible': False, 'rho': 0.99, 'score_duty_cycle': 0.1, 'score_each_iteration': False,
+                  'score_interval': 5, 'score_training_samples': 10000, 'score_validation_samples': 0,
+                  'score_validation_sampling': 'uniform', 'seed': -1, 'shuffle_training_data': False,
+                  'single_node_mode': False, 'sparse': False, 'sparsity_beta': 0, 'standardize': True,
+                  'stopping_metric': 'auto', 'stopping_rounds': 5, 'stopping_tolerance': 0,
+                  'target_ratio_comm_to_comp': 0.05, 'train_samples_per_iteration': -2, 'tweedie_power': 1.5,
+                  'use_all_factor_levels': True, 'variable_importances': True}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+
+class TestH2ORegressionAlgo(unittest.TestCase):
+
+    def setUp(self):
+        self.library = "h2o"
         self.service = "regression"
         pass
 
--- test/ml_tests/test_scikit_algo_assertions.py
+++ test/ml_tests/test_scikit_algo_assertions.py
@@ -51,6 +51,79 @@ class TestScikitClassificationAlgo(unittest.TestCase):
         # assertion should fail
         self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
 
+    def test_randomforest_1(self):
+        algorithm = "RandomForest"
+        params = {'bootstrap': True, 'ccp_alpha': 0.0, 'class_weight': None, 'criterion': 'gini', 'max_depth': None,
+                  'max_features': 'auto', 'max_leaf_nodes': None, 'max_samples': None, 'min_impurity_decrease': 0.0,
+                  'min_impurity_split': None, 'min_samples_leaf': 1, 'min_samples_split': 2,
+                  'min_weight_fraction_leaf': 0.0, 'n_estimators': 100, 'n_jobs': None, 'oob_score': False,
+                  'random_state': None, 'verbose': 0, 'warm_start': False}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_bagging_1(self):
+        algorithm = "Bagging"
+        params = {'base_estimator': None, 'bootstrap': True, 'bootstrap_features': False, 'max_features': 1.0,
+                  'max_samples': 1.0, 'n_estimators': 10, 'n_jobs': None, 'oob_score': False, 'random_state': None,
+                  'verbose': 0, 'warm_start': False}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_extratrees_1(self):
+        algorithm = "ExtraTrees"
+        params = {'bootstrap': False, 'ccp_alpha': 0.0, 'class_weight': None, 'criterion': 'gini', 'max_depth': None,
+                  'max_features': 'auto', 'max_leaf_nodes': None, 'max_samples': None, 'min_impurity_decrease': 0.0,
+                  'min_impurity_split': None, 'min_samples_leaf': 1, 'min_samples_split': 2,
+                  'min_weight_fraction_leaf': 0.0, 'n_estimators': 100, 'n_jobs': None, 'oob_score': False,
+                  'random_state': None, 'verbose': 0, 'warm_start': False}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_knearestneighbors_1(self):
+        algorithm = "KNearestNeighbors"
+        params = {'algorithm': 'auto', 'leaf_size': 30, 'metric': 'minkowski', 'metric_params': None, 'n_jobs': None,
+                  'n_neighbors': 5, 'p': 2, 'weights': 'uniform'}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_adaboost_1(self):
+        algorithm = "AdaBoost"
+        params = {'algorithm': 'SAMME.R', 'base_estimator': None, 'learning_rate': 1.0, 'n_estimators': 50,
+                  'random_state': None}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_naivebayesmultinomial_1(self):
+        algorithm = "NaiveBayesMultinomial"
+        params = {'alpha': 1.0, 'class_prior': None, 'fit_prior': True}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_gradientboostingmachines_1(self):
+        algorithm = "GradientBoostingMachines"
+        params = {'ccp_alpha': 0.0, 'criterion': 'friedman_mse', 'init': None, 'learning_rate': 0.1, 'loss': 'deviance',
+                  'max_depth': 3, 'max_features': None, 'max_leaf_nodes': None, 'min_impurity_decrease': 0.0,
+                  'min_impurity_split': None, 'min_samples_leaf': 1, 'min_samples_split': 2,
+                  'min_weight_fraction_leaf': 0.0, 'n_estimators': 100, 'n_iter_no_change': None, 'random_state': None,
+                  'subsample': 1.0, 'tol': 0.0001, 'validation_fraction': 0.1, 'verbose': 0, 'warm_start': False}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_xgradientboosting_1(self):
+        algorithm = "XGradientBoosting"
+        params = {'base_score': 0.5, 'booster': 'gbtree', 'colsample_bylevel': 1, 'colsample_bytree': 1, 'gamma': 0,
+                  'learning_rate': 0.1, 'max_delta_step': 0, 'max_depth': 3, 'min_child_weight': 1, 'missing': None,
+                  'n_estimators': 100, 'n_jobs': 1, 'nthread': None, 'objective': 'binary:logistic', 'random_state': 0,
+                  'reg_alpha': 0, 'reg_lambda': 1, 'scale_pos_weight': 1, 'seed': None, 'silent': True, 'subsample': 1}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_supportvectormachines_1(self):
+        algorithm = "SupportVectorMachines"
+        params = {'C': 1.0, 'break_ties': False, 'cache_size': 200, 'class_weight': None, 'coef0': 0.0,
+                  'decision_function_shape': 'ovr', 'degree': 3, 'gamma': 'scale', 'kernel': 'rbf', 'max_iter': -1,
+                  'probability': False, 'random_state': None, 'shrinking': True, 'tol': 0.001, 'verbose': False}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
+    def test_logisticregression_1(self):
+        algorithm = "LogisticRegression"
+        params = {'C': 1.0, 'class_weight': None, 'dual': False, 'fit_intercept': True, 'intercept_scaling': 1,
+                  'l1_ratio': None, 'max_iter': 100, 'multi_class': 'auto', 'n_jobs': None, 'penalty': 'l2',
+                  'random_state': None, 'solver': 'lbfgs', 'tol': 0.0001, 'verbose': 0, 'warm_start': False}
+        self.assertTrue(hyper_parameter_check(self.library, self.service, algorithm, params))
+
 
 class TestScikitRegressionAlgo(unittest.TestCase):
 
