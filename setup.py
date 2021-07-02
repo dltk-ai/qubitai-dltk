b1ad6214f3902ab30e2f501dd7d9056eea145a89
kavyavelagapudi <vkavya252@gmail.com>
2021-07-02 17:37:15 +0530

    Version 1.0.6 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qubitai-dltk",
-    version="1.0.5",
+    version="1.0.6",
     author="DLTK",
     install_requires=[
         "pandas==1.2.3",
